30d7d7cbd8705e7fc70a988e0b13050163124981
godwillcodes <iamgodwillb@gmail.com>
2024-02-01 01:56:24 +0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import { type Metadata } from 'next'
+
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Godwill Barasa',
+    default:
+      'Godwill Barasa - Software Engineer, Founder at NestBella and Web Consultant based in Nairobi, Kenya',
+  },
+  description:
+    'Godwill Barasa: Crafting Immersive Digital Experiences, One Line of Code at a Time!',
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
