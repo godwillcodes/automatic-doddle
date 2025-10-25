e5cd44141fc2534a79b17a8e0e51481a8468ed04
Godwill Barasa <33608094+godwillcodes@users.noreply.github.com>
2025-10-25 07:58:07 +0300

    Refactor sitemap.xml generation: Remove image and news namespaces for articles, simplifying the XML structure while maintaining essential page information.

--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -29,34 +29,16 @@ export async function GET() {
   const allPages = [...staticPages, ...articlePages]
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
-        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
-        xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   ${allPages
     .map((page) => {
       const url = `${baseUrl}${page.path}`
-      const isArticle = page.path.startsWith('/articles/')
       
       return `  <url>
     <loc>${url}</loc>
     <lastmod>${page.lastmod}</lastmod>
     <changefreq>${page.changefreq}</changefreq>
     <priority>${page.priority}</priority>
-    ${isArticle ? `
-    <image:image>
-      <image:loc>${baseUrl}/images/portrait.jpg</image:loc>
-      <image:title>${(page as any).article?.title || 'Article Image'}</image:title>
-      <image:caption>${(page as any).article?.description || 'Article featured image'}</image:caption>
-      <image:license>${baseUrl}</image:license>
-    </image:image>
-    <news:news>
-      <news:publication>
-        <news:name>Godwill Barasa</news:name>
-        <news:language>en</news:language>
-      </news:publication>
-      <news:publication_date>${page.lastmod}</news:publication_date>
-      <news:title>${(page as any).article?.title || ''}</news:title>
-    </news:news>` : ''}
   </url>`
     })
     .join('\n')}
